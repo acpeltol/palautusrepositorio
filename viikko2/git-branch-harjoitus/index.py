4dcb8373fd98ab2ba56dc39cd040be65a170ec03
acpeltol <apeltola717@gmial.com>
2024-11-04 15:33:42 +0200

    chages

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -9,16 +9,7 @@ logger("aloitetaan")
 x = int(input("luku 1: "))
 y = int(input("luku 2: "))
 
-<<<<<<< HEAD
-print(f"Lukujen {x} ja {y} summa on {summa(x, y)}")  # muutos bugikorjaus-branchissa
-print(f"Lukujen {x} ja {y} erotus on {erotus(x, y)}")  # muutos bugikorjaus-branchissa
-
-logger("lopetetaan")
-
-print("goodbye!") # lisäys bugikorjaus-branchissa
-=======
 print(f"{x} + {y} = {summa(x, y)}") # muutos mainissa
 print(f"{x} - {y} = {erotus(x, y)}") # muutos mainissa
 
 logger("lopetetaan")
->>>>>>> b4a8f41 (chages)
