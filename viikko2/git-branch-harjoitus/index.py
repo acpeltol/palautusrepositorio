2bd34e6217a6ff53207ce2095e8fb0ffdc56f921
acpeltol <apeltola717@gmial.com>
2024-11-05 10:14:38 +0200

    I added tulo file

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -3,6 +3,7 @@
 from logger import logger
 from summa import summa
 from erotus import erotus
+from tulo import tulo
 
 logger("aloitetaan")
 
@@ -11,5 +12,6 @@ y = int(input("luku 2: "))
 
 print(f"{summa(x, y)}")
 print(f"{erotus(x, y)}")
+print(f"{tulo(x,y)}")
 
 logger("lopetetaan")
